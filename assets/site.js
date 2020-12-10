83ae748d8cc98d5a9857e4e92b48f6254a1572c3
pictonzs <zpicton@hawk.iit.edu>
2020-12-10 16:11:02 -0600

    JS source fixes

--- assets/site.js
+++ assets/site.js
@@ -1,24 +1,25 @@
 (function() {
-  if (!('querySelector' in document)) {
-    return;
-  }
 
-  document.querySelector('html').classname = 'js';
-  
+if (!('querySelector' in document)) {
+  return;
+}
+
+document.querySelector('html').className = 'js';
+
   var asides = document.querySelectorAll('aside');
   var parents = document.querySelectorAll('.parent');
 
-  for (var i = 0; i < parents.length; i++){
-    parents[i].addEventListener('click', function(event)){
+  for (var i = 0; i < parents.length; i++) {
+    parents[i].addEventListener('click', function(event){
       var parent_node = event.target.parentNode;
       var aside = parent_node.querySelector('aside');
       if (aside.className === '' || aside.className === 'active'){
         aside.className = 'inactive';
-       } else {
-         aside.className='active';
+      } else {
+        aside.className = 'active';
       }
        console.log('Hey, you clicked a parent for an optional aside element', parent_node, aside.className);
-    });
-  }
+   });
+ }
 
-})
+ })();
