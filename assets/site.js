b066ae04b8c352ca3bf967587241305229a6c8c2
pictonzs <zpicton@hawk.iit.edu>
2020-12-10 15:32:47 -0600

    Make js safeties

--- assets/site.js
+++ assets/site.js
@@ -1,18 +1,24 @@
-document.querySelector('html').classname = 'js';
+(function() {
+  if (!('querySelector' in document)) {
+    return;
+  }
 
+  document.querySelector('html').classname = 'js';
+  
+  var asides = document.querySelectorAll('aside');
+  var parents = document.querySelectorAll('.parent');
 
-var asides = document.querySelectorAll('aside');
-var parents = document.querySelectorAll('.parent');
+  for (var i = 0; i < parents.length; i++){
+    parents[i].addEventListener('click', function(event)){
+      var parent_node = event.target.parentNode;
+      var aside = parent_node.querySelector('aside');
+      if (aside.className === '' || aside.className === 'active'){
+        aside.className = 'inactive';
+       } else {
+         aside.className='active';
+      }
+       console.log('Hey, you clicked a parent for an optional aside element', parent_node, aside.className);
+    });
+  }
 
-for (var i = 0; i < parents.length; i++){
-  parents[i].addEventListener('click', function(event)){
-    var parent_node = event.target.parentNode;
-    var aside = parent_node.querySelector('aside');
-    if (aside.className === '' || aside.className === 'active'){
-      aside.className = 'inactive';
-     } else {
-       aside.className='active';
-    }
-     console.log('Hey, you clicked a parent for an optional aside element', parent_node, aside.className);
-  });
-}
+})
