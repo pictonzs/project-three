1d8aaf13323266da06f5796922b169bdb748eff5
pictonzs <zpicton@hawk.iit.edu>
2020-12-10 15:05:57 -0600

    Make disapearing text prototype

--- assets/site.js
+++ assets/site.js
@@ -4,7 +4,11 @@ for (var i = 0; i < asides.length; i++){
   asides[i].addEventListener('click', function(event)){
     var parent_node = event.target.parentNode;
     var aside = parent_node.querySelector('aside');
-    console.log('Hey, you clicked an aside element')
-    aside.classname = 'active'
+    if (aside.className === '' || aside.className === 'active'){
+      aside.className = 'inactive';
+     } else {
+       aside.className='active';
+    }
+     console.log('Hey, you clicked an aside element', parent_node, aside.className)
   });
 }
